4c7748ccd01f6cce1b553fa830ae6e7e8f2d7352
Ceganjac <ceganjacaleksandar@gmail.com>
2025-04-04 12:57:58 +0200

    Ispravka main metode, sređeno skaliranje

--- VILAMET_PROGRAM/src/pogled/Main.java
+++ VILAMET_PROGRAM/src/pogled/Main.java
@@ -3,41 +3,35 @@ package pogled;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
-
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
 public class Main {
-	
-	// UZIMANJE DIMENZIJA EKRANA
-		static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int screenSirina = screenSize.width;
-		int screenVisina = screenSize.height;
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
 			try {
-				
-				System.setProperty("sun.java2d.opengl", "true");
+				// Fiksiranje skaliranja
+				System.setProperty("sun.java2d.uiScale", "1.0");
 
-				
+				// Uzimanje dimenzija ekrana
+				Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+				// Look & Feel
 				try {
-				    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-				    // Alternativno, možete koristiti Nimbus LookAndFeel
-				    // UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+					UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 				} catch (Exception e) {
-				    e.printStackTrace();
+					e.printStackTrace();
 				}
 
 				Vilamet frame = new Vilamet();
-				frame.setVisible(true);
 				frame.setSize(screenSize);
 				frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+				frame.setVisible(true);
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		});
 	}
-	
-
 }
